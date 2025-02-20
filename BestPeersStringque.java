74960da0b6854f11e0db162eaa3c877608ebadca
Ali khan <khanali6861@gmail.com>
2025-02-20 18:46:41 +0530

    Update

--- BestPeersStringque.java
+++ BestPeersStringque.java
@@ -1,6 +1,6 @@
 public class BestPeersStringque {
     public static void main(String[] args) {
-       String str="ABHIEP98043HKJDKJF83";
+       String str="ABHIE98043HKJDKJF83";
        StringBuilder sb=new StringBuilder();
        for(int i=str.length()-1;i>=0;i--){
         char ch=str.charAt(i);
